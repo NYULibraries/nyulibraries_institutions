d7cd663de25c845b5449c6f4e15f5c6cf6427e46
Eric Griffis <eric.griffis@nyu.edu>
2016-07-22 14:37:45 -0400

    Cleanup engine

--- lib/nyulibraries_institutions/engine.rb
+++ lib/nyulibraries_institutions/engine.rb
@@ -3,9 +3,6 @@ module NyulibrariesInstitutions
     # So we don't overlap in client namespace
     isolate_namespace NyulibrariesInstitutions
 
-    # Include the view path in the client application
-    ActionController::Base.append_view_path(File.join(root, 'app', 'templates', 'nyulibraries_templates'))
-
     # Include the helper in the client application
     config.to_prepare do
       ApplicationController.helper(NyulibrariesInstitutions::InstitutionHelper)
@@ -14,8 +11,6 @@ module NyulibrariesInstitutions
         include NyulibrariesInstitutions::InstitutionHelper
       end
     end
-
-
 
     config.generators do |g|
       g.test_framework :rspec, :fixture => false
