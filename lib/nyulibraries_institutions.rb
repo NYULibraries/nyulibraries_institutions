2613449aa853b3f0d35cb5e44a9f6accbf778a4d
Eric Griffis <eric.griffis@nyu.edu>
2016-08-01 16:14:48 -0400

    Add configuration

--- lib/nyulibraries_institutions.rb
+++ lib/nyulibraries_institutions.rb
@@ -6,6 +6,8 @@ module NyulibrariesInstitutions
     # modified from bootstrap-sass gem
     def load!
       register_rails_engine if rails?
+      # add common configuration to front of loadpath
+      Institutions.loadpaths.unshift(configuration_filepath)
     end
 
     def rails?
@@ -16,6 +18,10 @@ module NyulibrariesInstitutions
 
     def register_rails_engine
       require 'nyulibraries_institutions/engine'
+    end
+
+    def configuration_filepath
+      File.join(File.dirname(__FILE__), '../', 'config')
     end
   end
 end
